1f4b3c543b2e1cd5965718334518df1a8350e85a
Kevin Olschewsky <63854266+kolschew@users.noreply.github.com>
2022-04-05 14:11:40 +0200

    Update _full_nlo.py

--- InclusiveVcb/_full_nlo.py
+++ InclusiveVcb/_full_nlo.py
@@ -1,25 +1,25 @@
 """
 Description
 -----------
-    Module contains the full NLO contributions to the moments.
-    Since the NLO corrections from the scheme change are incorporated in the q2moment-files,
-    the expressions were reexpanded in a_s analytically.
+Module contains the full NLO contributions to the moments.
+Since the NLO corrections from the scheme change are incorporated in the q2moment-files,
+the expressions were reexpanded in a_s analytically.
 
 Methods
 -------
-    nlo_1,..., nlo_4
-        NLO contributions for the non-centralized moments
-    nlo_cent_1,..., nlo_cent_4
-        NLO contributions for the centralized moments
+nlo_1,..., nlo_4
+    NLO contributions for the non-centralized moments
+nlo_cent_1,..., nlo_cent_4
+    NLO contributions for the centralized moments
 
-Args
-----
-    q_cut : numpy.array 
-        Cuts for the lepton invariant mass q2
-    mbkin : float
-        Mass of the bottom quark in the kinetic scheme
-    mc : float
-        Mass of the charm quark mass in either the kinetic or MS-bar scheme
+Paramters
+---------
+q_cut : numpy.array 
+    Cuts for the lepton invariant mass q2
+mbkin : float
+    Mass of the bottom quark in the kinetic scheme
+mc : float
+    Mass of the charm quark mass in either the kinetic or MS-bar scheme
 """
 
 import numpy as np
